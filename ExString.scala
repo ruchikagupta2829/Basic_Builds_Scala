95cd788e1a1e6923a4b18a03db538a7bacf2e5f7
Ruchika Gupta <96807981+ruchikagupta2829@users.noreply.github.com>
2022-03-30 23:21:53 +0530

    Scala Basic Builds

---
+++ ExString.scala
@@ -0,0 +1,7 @@
+object ExString {
+  def main(args:Array[String]):Unit={
+    val text:String = "It is a Scala Programming";
+    println("Value of text is:"+text);
+  }
+
+}
---
+++ MulVar.scala
@@ -0,0 +1,11 @@
+object MulVar {
+  def main(args:Array[String]):Unit={
+    var (name:String, age:Int)=("Ruchi", 29);
+    var(address,mobile)=("Indore",9009123);
+    println("Name:"+name);
+    println("Age:"+age);
+    println("Address:"+address);
+    println("Mobile:"+mobile);
+  }
+
+}
